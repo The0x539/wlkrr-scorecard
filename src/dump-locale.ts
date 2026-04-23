e88e0e7b258cbacd2492417b2ad74d2955ee3326
The0x539 <the0x539@gmail.com>
2026-04-22 22:10:42 -0500

    add "select" category to locale dump

--- src/dump-locale.ts
+++ src/dump-locale.ts
@@ -4,7 +4,6 @@ import { Asset, BundleFile } from "./asset-bundle.ts";
 import { MonoBehaviour } from "./unity-asset/mono-behaviour.ts";
 import { BinaryReader, Decodable } from "./decode.ts";
 import * as fs from "@std/fs";
-import { Language, LocalizationDigest } from "./game-data/locale.ts";
 
 const steamDir = [
   "C:/Program Files (x86)/Steam",
@@ -60,25 +59,25 @@ for (const oi of asset.objectInfos) {
   behaviours.set(mb.name, mb);
 }
 
-const categories = ["system", "comment", "name", "present"];
+const categories = ["system", "comment", "name", "present", "select"];
 
 for (const category of categories) {
   Deno.mkdirSync(`src/game-data/locale/${category}/`, { recursive: true });
 
-  const groups: Partial<Record<Language, string[]>> = {};
+  const groups: Record<string, string[]> = {};
   const obj = behaviours.get(category)!;
   const payload = new BinaryReader(obj.payload);
   const datas = payload.array(payload.u32(), (r) => new TextCommon(r));
 
   for (const tc of datas) {
     for (const [language, text] of Object.entries(tc.texts)) {
-      groups[language as Language] ??= [];
-      groups[language as Language]!.push(text);
+      groups[language] ??= [];
+      groups[language].push(text);
     }
   }
 
   for (const [language, list] of Object.entries(groups)) {
     const path = `src/game-data/locale/${category}/${language}.txt`;
-    Deno.writeTextFileSync(path, list.join("\n"));
+    Deno.writeTextFileSync(path, list!.join("\n"));
   }
 }
